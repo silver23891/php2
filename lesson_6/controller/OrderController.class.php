5743033d229975751a3f2c4dcb760ac6b3d5a0dd
Anton <aborisov@yandex.ru>
2019-10-16 23:35:13 +0700

    Lesson 6

---
+++ lesson_6/controller/OrderController.class.php
@@ -0,0 +1,85 @@
+<?php
+
+class OrderController extends Controller
+{
+    public $view = 'order';
+    
+    public function add()
+    {   
+        $_GET['asAjax'] = true;
+        
+        $result = [
+            'result' => 0
+        ];
+        if (isset($_POST['id_good'])) {
+            $basket = [];
+            if (isset($_SESSION['basket'])) {
+                $basket = $_SESSION['basket'];
+            }
+            if (($index = array_search($_POST['id_good'], array_column($basket, 'id_good'))) !== false) {
+                $basket[$index]['quantity'] += 1;
+            } else {
+                $basket[] = [
+                    'id_good' => $_POST['id_good'],
+                    'name' => $_POST['name'],
+                    'price' => $_POST['price'],
+                    'quantity' => 1
+                ];
+            }
+            $_SESSION['basket'] = $basket;
+            
+            $result['result'] = 1;
+        }
+        
+        return json_encode($result);
+    }
+    
+    public function basket($data)
+    {
+        $basket = [];
+        if (isset($_SESSION['basket'])) {
+            $basket = $_SESSION['basket'];
+        }
+        return $basket;
+    }
+    
+    public function create($data)
+    {
+        if (isset($_POST['fio']) && isset($_SESSION['basket'])) {
+            $order_id = db::getInstance()->insert(
+                "insert into orders (fio, phone) values (:fio, :phone)",
+                [':fio' => $_POST['fio'], ':phone' => $_POST['phone']]
+            );
+            foreach ($_SESSION['basket'] as $good) {
+                $id = db::getInstance()->insert(
+                    "insert into order_goods (id_order, id_good, quantity) values (:id_order, :id_good, :quantity)",
+                    [':id_order' => $order_id, ':id_good' => $good['id_good'], ':quantity' => $good['quantity']]
+                );
+            }
+            unset($_SESSION['basket']);
+            return ['order' => 'Заказ успешно оформлен. Номер заказа - ' . $order_id];
+        }
+        return ['order' => 'При оформлении заказа произошла ошибка'];
+    }
+    
+    
+    /*public function add(){
+        $_GET['asAjax'] = true;
+
+        $result = [
+            'result' => 0
+        ];
+
+        $id_good = (int)$_POST['id_good'];
+        if($id_good > 0){
+            $basket = new Basket();
+            $basket->setIdGood($id_good);
+            $basket->setPrice(Good::getGoodPrice($id_good));
+            $basket->save();
+
+            $result['result'] = 1;
+        }
+
+        return json_encode($result);
+    }*/
+}
---
+++ lesson_6/lib/db.class.php
@@ -0,0 +1,86 @@
+<?php
+class db
+{
+    private static $_instance = null;
+
+    private $db; // Ресурс работы с БД
+
+    /*
+     * Получаем объект для работы с БД
+     */
+    public static function getInstance()
+    {
+        if (self::$_instance == null) {
+            self::$_instance = new db();
+        }
+        return self::$_instance;
+    }
+
+    /*
+     * Запрещаем копировать объект
+     */
+    private function __construct() {}
+    private function __sleep() {}
+    private function __wakeup() {}
+    private function __clone() {}
+
+    /*
+     * Выполняем соединение с базой данных
+     */
+    public function Connect($user, $password, $base, $host = 'localhost', $port = 3306)
+    {
+        // Формируем строку соединения с сервером
+        $connectString = 'mysql:host=' . $host . ';port= ' . $port . ';dbname=' . $base . ';charset=UTF8;';
+        $this->db = new PDO($connectString, $user, $password,
+            [
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // возвращать ассоциативные массивы
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION // возвращать Exception в случае ошибки
+            ]
+        );
+    }
+
+    /*
+     * Выполнить запрос к БД
+     */
+    public function Query($query, $params = array())
+    {
+        $res = $this->db->prepare($query);
+        $res->execute($params);
+        return $res;
+    }
+
+    /*
+     * Выполнить запрос с выборкой данных
+     */
+    public function Select($query, $params = array())
+    {
+        $result = $this->Query($query, $params);
+        if ($result) {
+            return $result->fetchAll();
+        }
+    }
+    
+    /**
+     *
+     * @param string $sql
+     * @param array $args
+     * @return \PDOStatement
+     */
+    private function sql($sql, $args = []) {
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute($args);
+        return $stmt;
+    }
+    
+    /**
+     *
+     * @param string $sql
+     * @param array $args
+     * @return integer ID
+     */
+    public function insert($sql, $args = []) {
+        $this->sql($sql, $args);
+        return $this->db->lastInsertId();
+    }
+}
+?>
